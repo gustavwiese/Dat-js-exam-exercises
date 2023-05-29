fe6a0c1dd98ab554591d28a201f8294e9716ec99
gustavwiese <gusse12345@gmail.com>
2023-05-29 10:13:39 +0200

    first 2 tasks done on excercise 01

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -1,2 +1,25 @@
 "use strict";
 
+window.addEventListener("load", start);
+
+async function start() {
+  console.log("js running");
+  const userList = await getUsers("users.json");
+  for (const user of userList) {
+    showUsers(user);
+  }
+}
+
+async function getUsers(url) {
+  const response = await fetch(url);
+  const data = response.json();
+  return data;
+}
+
+function showUsers(user) {
+  const html = /*html*/ `
+    <li>${user.name}</li>
+    <p>User active ${user.active}</p> 
+    `;
+  document.querySelector("#userlist").insertAdjacentHTML("beforeend", html);
+}
