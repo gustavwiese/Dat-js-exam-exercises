928a93095a4454d13bbd26bd42712921a46f779c
gustavwiese <gusse12345@gmail.com>
2023-05-29 09:16:53 +0200

    excercise 00 done

--- environments/environment-00/script.js
+++ environments/environment-00/script.js
@@ -1,1 +1,24 @@
 "use strict";
+
+window.addEventListener("load", start);
+
+function start() {
+  console.log("JavaScript kører");
+  RegisterClick();
+  hideResults();
+}
+
+function hideResults() {
+  document.querySelector("#result_failure").classList.add("hide");
+  document.querySelector("#result_success").classList.add("hide");
+}
+
+function RegisterClick() {
+  document.querySelector("#btn-knap").addEventListener("click", btnClicked);
+}
+
+function btnClicked() {
+  console.log("Det virker");
+  document.querySelector("#result_failure").classList.add("hide");
+  document.querySelector("#result_success").classList.remove("hide");
+}
