be56c005b530e39dd0200543ae755ae0a0904c2a
gustavwiese <gusse12345@gmail.com>
2023-05-31 10:25:32 +0200

    changed excercise 1

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -1,25 +1,29 @@
-"use strict";
+"use strict"; /*
+/* --- !!! ØVELSE 1 !!! --- */
+
+/*
 
 window.addEventListener("load", start);
 
 async function start() {
   console.log("js running");
-  const userList = await getUsers("users.json");
-  for (const user of userList) {
-    showUsers(user);
-  }
+  const users = await getUsers();
+  showUsers(users);
 }
 
-async function getUsers(url) {
-  const response = await fetch(url);
+async function getUsers() {
+  const response = await fetch("users.json");
   const data = response.json();
   return data;
 }
 
-function showUsers(user) {
-  const html = /*html*/ `
+function showUsers(users) {
+  for (const user of users) {
+    const html = `
     <li>${user.name}</li>
-    <p>User active ${user.active}</p> 
+    <p>User active: ${user.active}</p> 
+    <p>Role: ${user.role}</p>
     `;
-  document.querySelector("#userlist").insertAdjacentHTML("beforeend", html);
-}
+    document.querySelector("#userlist").insertAdjacentHTML("beforeend", html);
+  }
+}*/
