b6e6156c639419594c10a915a8f6bb41f3126396
gustavwiese <gusse12345@gmail.com>
2023-06-05 16:21:55 +0200

    finished all till 14

--- environments/environment-04/script.js
+++ environments/environment-04/script.js
@@ -1,1 +1,67 @@
 "use strict";
+
+window.addEventListener("load", start);
+
+/* --------------- !!! ØVELSE 9 !!! ------------------- */
+
+/*
+
+import { teachers } from "./teachers.js";
+
+function start() {
+  console.log(teachers);
+  showTeachers();
+}
+
+function showTeachers() {
+  teachers.sort(sortByName);
+  teachers.sort(sortByEmail);
+  for (const teacher of teachers) {
+    const html = `
+    <li>Name: ${teacher.name} - Email: ${teacher.email}</li>
+    `;
+    document.querySelector("#teachers-list").insertAdjacentHTML("beforeend", html);
+  }
+}
+
+function sortByName(a, b) {
+  return a.name.localeCompare(b.name);
+}
+
+function sortByEmail(a, b) {
+  return a.email.localeCompare(b.email);
+}
+
+*/
+
+/* --------------- !!! ØVELSE 10 !!! ------------------- */
+
+/*
+
+import { teachers } from "./teachers.js";
+
+function start() {
+  console.log(teachers);
+  showTeachers();
+}
+
+function showTeachers() {
+  for (const teacher of teachers) {
+    const html = `
+    <li>Name: ${teacher.name} - Email: ${teacher.email}</li>
+    `;
+    document.querySelector("#teachers-list").insertAdjacentHTML("beforeend", html);
+  }
+}
+
+function addTeacher(name, email) {
+  const teacher = {
+    name: name,
+    email: email,
+  };
+  teachers.push(teacher);
+}
+
+addTeacher("peter", "petl@kea.dk");
+
+*/
--- environments/environment-05/script.js
+++ environments/environment-05/script.js
@@ -1,1 +1,109 @@
 "use strict";
+
+/* --------------- !!! ØVELSE 11 !!! ------------------- */
+
+window.addEventListener("load", start);
+
+/*
+
+
+import { courses } from "./courses.js";
+
+function start() {
+  console.log(courses);
+  showCourses();
+}
+
+function showCourses() {
+  for (const course of courses) {
+    const html = `
+    <li>Name: ${course.name} -
+    ECTS point: ${course.ectsPoints} -
+    Teacher: ${course.teacher}</li>
+    `;
+    document.querySelector("#courses-list").insertAdjacentHTML("beforeend", html);
+  }
+}
+
+function addCourse(name, ectsPoints, teacher) {
+  const course = {
+    name: name,
+    ectsPoints: ectsPoints,
+    teacher: teacher,
+  };
+  courses.push(course);
+}
+
+addCourse("livredning", 10000, "Gustav");
+
+*/
+
+/* --------------- !!! ØVELSE 12 !!! ------------------- */
+
+/*
+
+import { courses } from "./courses.js";
+
+function start() {
+  console.log(courses);
+  showCourses();
+}
+
+function showCourses() {
+  courses.sort((a, b) => a.ectsPoints - b.ectsPoints);
+  for (const course of courses) {
+    const html = `
+    <li>Name: ${course.name} - ECTS Points: ${course.ectsPoints}</li>
+    `;
+    document.querySelector("#courses-list").insertAdjacentHTML("beforeend", html);
+  }
+}
+
+*/
+
+/* --------------- !!! ØVELSE 13 !!! ------------------- */
+
+/*
+
+import { courses } from "./courses.js";
+
+function start() {
+  console.log(courses);
+  showCourses();
+}
+
+function showCourses() {
+  for (const course of courses) {
+    const html = `
+    <li>Name: ${course.name} - ECTS Points: ${course.ectsPoints}</li>
+    `;
+    document.querySelector("#courses-list").insertAdjacentHTML("beforeend", html);
+  }
+}
+
+// HER SKAL LAVES EN FUNKTION DER SORTERER EFTER VALGTE OPTION I HTML-FILEN
+
+*/
+
+/* --------------- !!! ØVELSE 14 !!! ------------------- */
+
+/*
+
+import { courses } from "./courses.js";
+
+function start() {
+  console.log(courses);
+  showCourses();
+}
+
+function showCourses() {
+  courses.sort((a, b) => new Date(a.startDate) - new Date(b.startDate));
+  for (const course of courses) {
+    const html = `
+    <li>Name: ${course.name} - Start Date: ${course.startDate} - ECTS Points: ${course.ectsPoints}</li>
+    `;
+    document.querySelector("#courses-list").insertAdjacentHTML("beforeend", html);
+  }
+}
+
+*/
